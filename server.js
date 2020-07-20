6583847804406becd8a551d3055a6b8565c629b4
Lance Page <31941666+6ftdotexe@users.noreply.github.com>
2020-07-19 21:54:03 -0500

    server update

--- server.js
+++ server.js
@@ -1,25 +1,26 @@
-// Server
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes/apiRoutes");
 const htmlRoutes = require("./routes/htmlRoutes");
 
+const PORT = process.env.PORT || 7000;
+
 const app = express();
-const PORT = 8000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
 
-var MONGODB_URI = "mongodb://localhost/workout";
+app.use(express.static("public"));
+
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 mongoose.connect(MONGODB_URI, {
-    useNewUrlParser: true,
-    useFindAndModify: false
-})
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
 app.use(routes);
 app.use(htmlRoutes);
 
 app.listen(PORT, () => {
-    console.log("App listening on Port " + PORT);
-}); 
+  console.log(`App running on port ${PORT}`);
+});
