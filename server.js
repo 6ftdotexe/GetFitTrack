a740e5438aa2924173d5439654c9f26b3925e1c2
Lance Page <31941666+6ftdotexe@users.noreply.github.com>
2020-07-19 20:55:18 -0500

    Local Server created

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
 // Server
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
+
+const app = express();
+const PORT = 8000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+var MONGODB_URI = "mongodb://localhost/workout";
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: false
+})
+
+app.use(routes);
+app.use(htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log("App listening on Port " + PORT);
+}); 
